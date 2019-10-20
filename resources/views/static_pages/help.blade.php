9dc846bd9fc38279fa73d5abe2501416db5d5382
Tan DuZhi <lucky.tanduzhi@gmail.com>
2019-10-20 22:37:03 +0800

    静态页面，定义路由以及生成控制

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts/default')
+
+@section('title', "帮助页")
+
+@section('content')
+    <h2>帮帮我吧</h2>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/default')
+
+@section('content')
+    <h1>主页要炫酷</h1>
+@stop
