f6eec1a7c24f9d2b353a630b79d1088c2abbfc07
vagrant <vagrant@homestead>
2019-10-21 14:07:09 +0000

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,16 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-@yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('layouts._footer')
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
