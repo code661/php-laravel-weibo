cdaa006af3534855cf043219babb246e657d4b54
Tan DuZhi <lucky.tanduzhi@gmail.com>
2019-11-11 18:12:01 +0800

    用户注册表单

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/dev/weibo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/dev/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/tan61/dev/weibo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/tan61/dev/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
