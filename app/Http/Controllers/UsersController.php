52a754388db98e0d72004491ef1463efc593a931
Tan DuZhi <lucky.tanduzhi@gmail.com>
2019-11-11 20:53:12 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,4 +14,13 @@ class UsersController extends Controller
     public function show(User $user) {
         return view('users.show', compact('user'));
     }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
 }
